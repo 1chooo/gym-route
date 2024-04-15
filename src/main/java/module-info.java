d7e3aa691a7b92e097dc5a50374525ace89a34d7
1chooo <hugo970217@gmail.com>
2024-04-16 01:32:22 +0800

    style: xml loader, module-info warnings

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,9 @@ module stu_109601003.finalproject {
   requires org.controlsfx.controls;
   requires com.dlsc.formsfx;
   requires org.kordamp.bootstrapfx.core;
+  requires transitive javafx.graphics;
+  
+
 
   opens stu_109601003.finalproject to javafx.fxml;
   exports stu_109601003.finalproject;
--- src/main/java/stu_109601003/finalproject/Main.java
+++ src/main/java/stu_109601003/finalproject/Main.java
@@ -1,9 +1,3 @@
-/**
- *  This project is CE1002 Final Project
- *  and the name of this project is
- *
- */
-
 package stu_109601003.finalproject;
 
 import javafx.application.Application;
@@ -24,20 +18,20 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    FXMLLoader fxmlLoader1 = new FXMLLoader(Main.class.getResource("login.fxml"));
-    FXMLLoader fxmlLoader2 = new FXMLLoader(Main.class.getResource("home.fxml"));
-    FXMLLoader fxmlLoader3 = new FXMLLoader(Main.class.getResource("curriculum.fxml"));
-    FXMLLoader fxmlLoader4 = new FXMLLoader(Main.class.getResource("calendar.fxml"));
-    FXMLLoader fxmlLoader5 = new FXMLLoader(Main.class.getResource("rule.fxml"));
-    FXMLLoader fxmlLoader6 = new FXMLLoader(Main.class.getResource("preview.fxml"));
+    FXMLLoader loginLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+    FXMLLoader homeLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
+    FXMLLoader curriculumLoader = new FXMLLoader(Main.class.getResource("curriculum.fxml"));
+    FXMLLoader calendarLoader = new FXMLLoader(Main.class.getResource("calendar.fxml"));
+    FXMLLoader ruleLoader = new FXMLLoader(Main.class.getResource("rule.fxml"));
+    FXMLLoader previewLoader = new FXMLLoader(Main.class.getResource("preview.fxml"));
 
     currentStage = primaryStage;
-    loginScene = new Scene(fxmlLoader1.load());
-    homeScene = new Scene(fxmlLoader2.load());
-    curriculumScene = new Scene(fxmlLoader3.load());
-    calendarScene = new Scene(fxmlLoader4.load());
-    ruleScene = new Scene(fxmlLoader5.load());
-    previewScene = new Scene(fxmlLoader6.load());
+    loginScene = new Scene(loginLoader.load());
+    homeScene = new Scene(homeLoader.load());
+    curriculumScene = new Scene(curriculumLoader.load());
+    calendarScene = new Scene(calendarLoader.load());
+    ruleScene = new Scene(ruleLoader.load());
+    previewScene = new Scene(previewLoader.load());
     currentStage.setTitle("施伯伯動起來！！！");
     currentStage.setScene(loginScene);
 
